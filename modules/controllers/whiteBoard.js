daf899ebe98ff69d425f0abeae818c6250d6c6df
Jiahonzheng <jiahonzheng@gmail.com>
2017-11-25 16:50:26 +0800

    后端使用socket.io的rooms模式

--- modules/controllers/whiteBoard.js
+++ modules/controllers/whiteBoard.js
@@ -4,63 +4,62 @@ var encrypter = require('../utils/encryptor');
 var whiteBoard = [];
 
 function service(server) {
-	var io = socketIO.listen(server);
+    var io = socketIO.listen(server);
 
-	io.on('connection', function(socket) {
-		var canvasId;
+    io.on('connection', function (socket) {
+        var canvasId;
 
-		socket.on('set_id', function(data) {
-			canvasId = encrypter(data.id);
+        socket.on('set_id', function (data) {
+            canvasId = encrypter(data.id);
 
-			if (whiteBoard[canvasId] === undefined) {
-				whiteBoard[canvasId] = Object();
-				whiteBoard[canvasId].lineHistory = [];
-				whiteBoard[canvasId].dotHistory = [];
-			}
+            if (whiteBoard[canvasId] === undefined) {
+                whiteBoard[canvasId] = Object();
+                whiteBoard[canvasId].lineHistory = [];
+                whiteBoard[canvasId].dotHistory = [];
+            }
 
-			socket.emit('set_id_success', {
-				id: canvasId
-			});
+            socket.join(canvasId);
 
-			socket.on('roll_back', function(data) {
-				socket.emit('draw_dot' + canvasId, {
-					dot: whiteBoard[canvasId].dotHistory
-				});
+            socket.emit('set_id_success', {});
 
-				socket.emit('draw_line' + canvasId, {
-					line: whiteBoard[canvasId].lineHistory
-				});
-			});
+            socket.on('roll_back', function (data) {
+                socket.emit('draw_dot', {
+                    dot: whiteBoard[canvasId].dotHistory
+                });
 
-			socket.on('draw_dot' + canvasId, function(data) {
-				socket.broadcast.emit('draw_dot' + canvasId, {
-					dot: data.dot
-				});
+                socket.emit('draw_line', {
+                    line: whiteBoard[canvasId].lineHistory
+                });
+            });
 
-				for (var i = 0; i < data.dot.length; i++) {
-					whiteBoard[canvasId].dotHistory.push(data.dot[i]);
-				}
-			});
+            socket.on('draw_dot', function (data) {
+                socket.to(canvasId).emit('draw_dot', {
+                    dot: data.dot
+                });
 
-			socket.on('draw_line' + canvasId, function(data) {
-				socket.broadcast.emit('draw_line' + canvasId, {
-					line: data.line
-				});
+                for (var i = 0; i < data.dot.length; i++) {
+                    whiteBoard[canvasId].dotHistory.push(data.dot[i]);
+                }
+            });
 
-				for (var i = 0; i < data.line.length; i++) {
-					whiteBoard[canvasId].lineHistory.push(data.line[i]);
-				}
-			});
+            socket.on('draw_line', function (data) {
+                socket.to(canvasId).emit('draw_line', {
+                    line: data.line
+                });
 
-			socket.on('clear' + canvasId, function(data) {
-				socket.broadcast.emit('clear' + canvasId, {});
+                for (var i = 0; i < data.line.length; i++) {
+                    whiteBoard[canvasId].lineHistory.push(data.line[i]);
+                }
+            });
 
-				whiteBoard[canvasId].lineHistory = [];
-				whiteBoard[canvasId].dotHistory = [];
-			});
-		});
+            socket.on('clear', function (data) {
+                socket.to(canvasId).emit('clear', {});
 
-	});
+                whiteBoard[canvasId].lineHistory = [];
+                whiteBoard[canvasId].dotHistory = [];
+            });
+        });
+    });
 }
 
 module.exports = service;
--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ function fadeIn(element, time) {
     if (element.style.opacity === "") {
         element.style.opacity = 0;
     }
+
     if (element.style.display === "" || element.style.display === 'none') {
         element.style.display = 'block';
     }
@@ -19,6 +20,7 @@ function fadeOut(element, time) {
     if (element.style.opacity === "") {
         element.style.opacity = 1;
     }
+
     if (element.style.display === "" || element.style.display === 'none') {
         element.style.display = 'block';
     }
@@ -33,304 +35,323 @@ function fadeOut(element, time) {
     }, time / 100);
 }
 
-
 var socket = io.connect();
 
-var canvasId;
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("canvasIdInput").onkeyup = function (e) {
+        e = e || window.event;
 
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("canvasIdInput").onkeyup = function(e) {
-		e = e || window.event;
-		if (e.keyCode === 13) {
-			socket.emit("set_id", {
-				id: document.getElementById("canvasIdInput").value
-			});
-		}
-	};
-	var constant = {
-		minAngleChange: Math.PI / 180,
-		wheelSpeed: 0.01,
-		pointSize: 5,
-		penSize: 1,
-		color: '#000000',
-		offsetX: 0,
-		offsetY: 0
-	};
+        if (e.keyCode === 13) {
+            socket.emit("set_id", {
+                id: document.getElementById("canvasIdInput").value
+            });
+        }
+    };
+	
+    var constant = {
+        minAngleChange: Math.PI / 180,
+        wheelSpeed: 0.01,
+        pointSize: 5,
+        penSize: 1,
+        color: '#000000',
+        offsetX: 0,
+        offsetY: 0
+    };
 
-	var mouse = {
-		rightClick: false,
-		click: false,
-		move: false,
-		pos: { x: 0, y: 0 },
-		pos_prev: false,
-		pos_turn: false,
-		out: false,
-		distance: 0
-	};
-	// get canvas element and create context
-	var clearButton = document.getElementById('clear');
+    var mouse = {
+        rightClick: false,
+        click: false,
+        move: false,
+        pos: {x: 0, y: 0},
+        pos_prev: false,
+        pos_turn: false,
+        out: false,
+        distance: 0
+    };
+    
+    var clearButton = document.getElementById('clear');
 
-	var background = document.getElementById('background');
-	var contextBG = background.getContext('2d');
+    var background = document.getElementById('background');
+    var contextBG = background.getContext('2d');
+    var canvas = document.getElementById('drawing');
+    var context = canvas.getContext('2d');
 
-	var canvas = document.getElementById('drawing');
-	var context = canvas.getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	var socket = io.connect();
+    var linePoint = [];
+    var lineHistory = [];
+    var dotHistory = [];
 
-	var linePoint = [];
-	var line_history = [];
-	var dot_history = [];
+    socket.on('set_id_success', function (data) {
+        document.getElementById("board").style.display = "block";
+        fadeOut(document.getElementById("form"), 500);
+        fadeIn(document.getElementById("background"), 500);
 
+        socket.emit('roll_back', {});
+    });
 
+    socket.on('draw_line', function (data) {
+        var line = data.line;
 
-	// draw line received from server
-	socket.on('set_id_success', function(data) {
-		canvasId = data.id;
-		document.getElementById("board").style.display = "block";
-		fadeOut(document.getElementById("form"), 500);
-		fadeIn(document.getElementById("background"), 500);
-		socket.emit('roll_back', {});
+        for (var i = 0; i < line.length; i++) {
+            lineHistory.push(line[i]);
+        }
 
-		socket.on('draw_line' + canvasId, function(data) {
-			var line = data.line;
-			for (var i = 0; i < line.length; i++) {
-				line_history.push(line[i]);
-			}
+        redraw();
+    });
 
-			redraw();
-		});
+    socket.on('draw_dot', function (data) {
+        var dot = data.dot;
 
-		socket.on('draw_dot' + canvasId, function(data) {
-			var dot = data.dot;
+        for (var i = 0; i < dot.length; i++) {
+            dotHistory.push(dot[i]);
+            dotTo(contextBG, dot[i]);
+        }
+    });
 
-			for (var i = 0; i < dot.length; i++) {
-				dot_history.push(dot[i]);
-				dotTo(contextBG, dot[i]);
-			}
+    socket.on('clear', function (data) {
+        contextBG.clearRect(0, 0, width, height);
+        lineHistory = [];
+        dotHistory = [];
+    });
 
-		});
+    clearButton.onclick = function () {
+        contextBG.clearRect(0, 0, width, height);
+        lineHistory = [];
+        dotHistory = [];
+        socket.emit('clear', {});
+    };
 
-		socket.on('clear' + canvasId, function(data) {
-			contextBG.clearRect(0, 0, width, height);
-			line_history = [];
-			dot_history = [];
-		});
-	});
+    canvas.onmousewheel = function (e) {
+        if (mouse.click || mouse.rightClick) {
+            return;
+        }
 
-	clearButton.onclick = function() {
-		contextBG.clearRect(0, 0, width, height);
-		line_history = [];
-		dot_history = [];
-		socket.emit('clear' + canvasId, {});
-	};
+        var delta = e.detail || e.wheelDelta;
 
-	canvas.onmousewheel = function(e) {
-		if (mouse.click || mouse.rightClick)
-			return;
+        constant.penSize += delta * constant.wheelSpeed;
+        context.clearRect(0, 0, width, height);
+        drawCursor(e.clientX, e.clientY);
+    };
 
-		var delta = e.detail || e.wheelDelta;
-		constant.penSize += delta * constant.wheelSpeed;
-		context.clearRect(0, 0, width, height);
-		drawCursor(e.clientX, e.clientY);
-	};
+    canvas.onmousedown = function (e) {
+        e.preventDefault();
 
-	// register mouse event handlers
-	canvas.onmousedown = function(e) {
-		e.preventDefault();
-		switch (e.which) {
-			case 1:
-				linePoint = [];
-				context.lineWidth = constant.penSize;
-				context.color = constant.color;
-				mouse.click = true;
-				mouse.dot = true;
-				mouse.pos.x = e.clientX;
-				mouse.pos.y = e.clientY;
-				mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
-				mouse.pos_turn = false;
-				mouse.distance = 0;
-				putPoint(mouse.pos.x, mouse.pos.y);
-				linePoint[0].color = constant.color;
-				linePoint[0].penSize = constant.penSize;
-				break;
-			case 3:
-				mouse.rightClick = true;
-				mouse.pos_prev = false;
-				if (mouse.click) endLineDraw();
-				break;
-		}
+        switch (e.which) {
+            case 1:
+                linePoint = [];
+                context.lineWidth = constant.penSize;
+                context.color = constant.color;
+                mouse.click = true;
+                mouse.dot = true;
+                mouse.pos.x = e.clientX;
+                mouse.pos.y = e.clientY;
+                mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+                mouse.pos_turn = false;
+                mouse.distance = 0;
+                putPoint(mouse.pos.x, mouse.pos.y);
+                linePoint[0].color = constant.color;
+                linePoint[0].penSize = constant.penSize;
+                break;
+            case 3:
+                mouse.rightClick = true;
+                mouse.pos_prev = false;
+                if (mouse.click) endLineDraw();
+                break;
+        }
 
-	};
-	canvas.onmouseup = function(e) {
-		if (e.which == 3)
-			mouse.rightClick = false;
-		endLineDraw();
-	};
-	canvas.onmouseout = function(e) {
-		mouse.rightClick = false;
-		mouse.out = true;
-		endLineDraw();
-	};
-	canvas.oncontextmenu = function(e) { e.preventDefault(); };
+    };
 
-	canvas.onmousemove = function(e) {
-		mouse.out = false;
-		context.clearRect(0, 0, width, height);
-		drawCursor(e.clientX, e.clientY);
-		if (mouse.rightClick) {
-			if (mouse.pos_prev) {
-				var dx = mouse.pos.x - e.clientX;
-				var dy = mouse.pos.y - e.clientY;
-				constant.offsetX += dx;
-				constant.offsetY += dy;
-				redraw();
-			}
-			mouse.pos_prev = true;
-			mouse.pos.x = e.clientX;
-			mouse.pos.y = e.clientY;
-		} else if (mouse.click && mouse.pos_prev) {
-			mouse.distance += distance(mouse.pos, { x: e.clientX, y: e.clientY });
-			mouse.pos.x = e.clientX;
-			mouse.pos.y = e.clientY;
-			var dis = distance(mouse.pos, mouse.pos_prev);
+    canvas.onmouseup = function (e) {
+        if (e.which == 3)
+            mouse.rightClick = false;
 
-			if (dis >= constant.pointSize) {
-				if (!mouse.pos_turn)
-					mouse.pos_turn = { x: mouse.pos.x, y: mouse.pos.y };
+        endLineDraw();
+    };
 
+    canvas.onmouseout = function (e) {
+        mouse.rightClick = false;
+        mouse.out = true;
+        endLineDraw();
+    };
 
-				mouse.dot = false;
-				if (dis <= mouse.distance - constant.pointSize * 2 || hasPenChange(mouse.pos, mouse.pos_prev, mouse.pos_turn)) {
-					putPoint(mouse.pos.x, mouse.pos.y);
-					line(context, linePoint);
-					mouse.pos_turn = false;
-					mouse.distance = 0;
-					mouse.pos_prev.x = mouse.pos.x;
-					mouse.pos_prev.y = mouse.pos.y;
-				} else
-					line(context, linePoint, mouse.pos);
+    canvas.oncontextmenu = function (e) {
+        e.preventDefault();
+    };
 
-			} else if (linePoint.length > 0) {
-				line(context, linePoint, mouse.pos);
-			}
+    canvas.onmousemove = function (e) {
+        mouse.out = false;
+        context.clearRect(0, 0, width, height);
+        drawCursor(e.clientX, e.clientY);
 
-		}
+        if (mouse.rightClick) {
+            if (mouse.pos_prev) {
+                var dx = mouse.pos.x - e.clientX;
+                var dy = mouse.pos.y - e.clientY;
+                constant.offsetX += dx;
+                constant.offsetY += dy;
+                redraw();
+            }
+            mouse.pos_prev = true;
+            mouse.pos.x = e.clientX;
+            mouse.pos.y = e.clientY;
+        } else if (mouse.click && mouse.pos_prev) {
+            mouse.distance += distance(mouse.pos, {x: e.clientX, y: e.clientY});
+            mouse.pos.x = e.clientX;
+            mouse.pos.y = e.clientY;
+            var dis = distance(mouse.pos, mouse.pos_prev);
 
-	};
+            if (dis >= constant.pointSize) {
+                if (!mouse.pos_turn) {
+                    mouse.pos_turn = {x: mouse.pos.x, y: mouse.pos.y};
+                }
 
-	function endLineDraw() {
-		if (!mouse.click) return;
+                mouse.dot = false;
 
-		mouse.click = false;
+                if (dis <= mouse.distance - constant.pointSize * 2 || hasPenChange(mouse.pos, mouse.pos_prev, mouse.pos_turn)) {
+                    putPoint(mouse.pos.x, mouse.pos.y);
+                    line(context, linePoint);
+                    mouse.pos_turn = false;
+                    mouse.distance = 0;
+                    mouse.pos_prev.x = mouse.pos.x;
+                    mouse.pos_prev.y = mouse.pos.y;
+                } else
+                    line(context, linePoint, mouse.pos);
 
-		context.clearRect(0, 0, width, height);
-		if (!mouse.pos_turn && mouse.dot) {
-			dotTo(contextBG, { x: mouse.pos_prev.x, y: mouse.pos_prev.y });
-			socket.emit('draw_dot' + canvasId, {
-				dot: [{ x: mouse.pos_prev.x + constant.offsetX, y: mouse.pos_prev.y + constant.offsetY }]
-			});
-		} else if (!mouse.dot) {
-			contextBG.strokeStyle = constant.color;
-			contextBG.lineWidth = constant.penSize;
-			line(contextBG, linePoint, mouse.pos);
-			putPoint(mouse.pos.x, mouse.pos.y);
-		}
+            } else if (linePoint.length > 0) {
+                line(context, linePoint, mouse.pos);
+            }
 
-		if (linePoint.length > 1) {
-			socket.emit('draw_line' + canvasId, { line: [linePoint] });
-			line_history.push(linePoint);
-		}
-	}
+        }
 
-	function hasPenChange(now, prev, turn) {
-		if (now && prev && turn) {
-			var dx = now.x - prev.x;
-			var dy = now.y - prev.y;
-			var tdx = turn.x - prev.x;
-			var tdy = turn.y - prev.y;
+    };
 
-			var dis1 = distance(now, prev);
-			var dis2 = distance(turn, prev);
-			if (!dis1 && !dis2) return false;
+    function endLineDraw() {
+        if (!mouse.click) return;
 
-			var cos = (dx * tdx + dy * tdy) / (dis1 * dis2);
-			return (cos < 0 || Math.abs(cos) < Math.cos(constant.minAngleChange));
-		} else
-			return false;
-	}
+        mouse.click = false;
 
-	function distance(start, end) {
-		var dx = start.x - end.x;
-		var dy = start.y - end.y;
-		return Math.sqrt(dx * dx + dy * dy);
-	}
+        context.clearRect(0, 0, width, height);
 
-	function line(context, points, temp) {
-		if (!points && points.length <= 0)
-			return;
+        if (!mouse.pos_turn && mouse.dot) {
+            dotTo(contextBG, {x: mouse.pos_prev.x, y: mouse.pos_prev.y});
+            socket.emit('draw_dot', {
+                dot: [{x: mouse.pos_prev.x + constant.offsetX, y: mouse.pos_prev.y + constant.offsetY}]
+            });
+        } else if (!mouse.dot) {
+            contextBG.strokeStyle = constant.color;
+            contextBG.lineWidth = constant.penSize;
+            line(contextBG, linePoint, mouse.pos);
+            putPoint(mouse.pos.x, mouse.pos.y);
+        }
 
-		context.lineJoin = "round";
-		context.lineCap = "round";
-		context.beginPath();
-		context.moveTo(points[0].x - constant.offsetX, points[0].y - constant.offsetY);
-		for (var j = 1; j < points.length; j++)
-			context.lineTo(points[j].x - constant.offsetX, points[j].y - constant.offsetY);
+        if (linePoint.length > 1) {
+            socket.emit('draw_line', {line: [linePoint]});
+            lineHistory.push(linePoint);
+        }
+    }
 
-		if (temp)
-			context.lineTo(temp.x, temp.y);
+    function hasPenChange(now, prev, turn) {
+        if (now && prev && turn) {
+            var dx = now.x - prev.x;
+            var dy = now.y - prev.y;
+            var tdx = turn.x - prev.x;
+            var tdy = turn.y - prev.y;
+            var dis1 = distance(now, prev);
+            var dis2 = distance(turn, prev);
 
-		context.stroke();
-	}
+            if (!dis1 && !dis2) {
+                return false;
+            }
 
-	function dotTo(context, dot) {
-		context.fillStyle = constant.color;
-		context.beginPath();
-		context.arc(dot.x, dot.y, constant.penSize * 0.5, 0, 2 * Math.PI);
-		context.fill();
-	}
+            var cos = (dx * tdx + dy * tdy) / (dis1 * dis2);
 
-	function drawCursor(cursorX, cursorY) {
-		if (mouse.out) return;
-		context.save();
-		context.lineWidth = 1;
-		context.beginPath();
-		context.arc(cursorX, cursorY, Math.max(constant.penSize * 0.5 + 2.5, 7.5), 0, 2 * Math.PI);
-		context.stroke();
-		dotTo(context, { x: cursorX, y: cursorY });
-		context.restore();
-	}
+            return (cos < 0 || Math.abs(cos) < Math.cos(constant.minAngleChange));
+        } else
+            return false;
+    }
 
-	function redraw() {
-		contextBG.clearRect(0, 0, width, height);
-		contextBG.beginPath();
-		var i;
-		for (i = 0; i < line_history.length; i++) {
-			contextBG.strokeStyle = line_history[i][0].color;
-			contextBG.lineWidth = line_history[i][0].penSize;
-			line(contextBG, line_history[i]);
-		}
+    function distance(start, end) {
+        var dx = start.x - end.x;
+        var dy = start.y - end.y;
 
-		for (i = 0; i < dot_history.length; i++)
-			dotTo(contextBG, dot_history[i]);
-	}
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 
-	function putPoint(posX, posY) {
-		linePoint.push({ x: posX + constant.offsetX, y: posY + constant.offsetY });
-	}
+    function line(context, points, temp) {
+        if (!points && points.length <= 0) {
+            return;
+        }
 
-	// set canvas to full browser width/height
-	window.onresize = function() {
-		width = window.innerWidth;
-		height = window.innerHeight;
-		background.width = width;
-		background.height = height;
-		canvas.width = width;
-		canvas.height = height;
-		redraw();
-	};
+        context.lineJoin = "round";
+        context.lineCap = "round";
+        context.beginPath();
+        context.moveTo(points[0].x - constant.offsetX, points[0].y - constant.offsetY);
 
-	window.onresize();
+        for (var j = 1; j < points.length; j++) {
+            context.lineTo(points[j].x - constant.offsetX, points[j].y - constant.offsetY);
+        }
+
+        if (temp) {
+            context.lineTo(temp.x, temp.y);
+        }
+
+        context.stroke();
+    }
+
+    function dotTo(context, dot) {
+        context.fillStyle = constant.color;
+        context.beginPath();
+        context.arc(dot.x, dot.y, constant.penSize * 0.5, 0, 2 * Math.PI);
+        context.fill();
+    }
+
+    function drawCursor(cursorX, cursorY) {
+        if (mouse.out) {
+            return;
+        }
+
+        context.save();
+        context.lineWidth = 1;
+        context.beginPath();
+        context.arc(cursorX, cursorY, Math.max(constant.penSize * 0.5 + 2.5, 7.5), 0, 2 * Math.PI);
+        context.stroke();
+        dotTo(context, {x: cursorX, y: cursorY});
+        context.restore();
+    }
+
+    function redraw() {
+        contextBG.clearRect(0, 0, width, height);
+        contextBG.beginPath();
+
+        var i;
+
+        for (i = 0; i < lineHistory.length; i++) {
+            contextBG.strokeStyle = lineHistory[i][0].color;
+            contextBG.lineWidth = lineHistory[i][0].penSize;
+            line(contextBG, lineHistory[i]);
+        }
+
+        for (i = 0; i < dotHistory.length; i++) {
+            dotTo(contextBG, dotHistory[i]);
+            dotTo(contextBG, dotHistory[i]);
+        }
+    }
+
+    function putPoint(posX, posY) {
+        linePoint.push({x: posX + constant.offsetX, y: posY + constant.offsetY});
+    }
+
+    window.onresize = function () {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        background.width = width;
+        background.height = height;
+        canvas.width = width;
+        canvas.height = height;
+        redraw();
+    };
+
+    window.onresize();
 });
